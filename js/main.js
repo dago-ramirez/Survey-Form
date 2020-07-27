04ed81a840db928cb7b88f147d3445533dc799a5
Dago R <dagoramirezserna@gmail.com>
2020-07-27 14:32:07 -0500

    Se ensaya la validación del  correo con js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// document.getElementById("email").addEventListener("change", validarEmail)
+
+// function validarEmail() {
+//     var emailInput = document.getElementById("email").value;
+//     if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(emailInput))) {
+//       alert("La dirección de email es incorrecta.");
+//     }
+}
